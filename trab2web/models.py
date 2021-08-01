e6c7bc2b71ab160f341cd92658dfd10ae868ab3f
Larisse <larisse16rocha@gmail.com>
2021-07-31 21:59:09 -0300

    Modificações nos arquivos

--- trab2web/models.py
+++ trab2web/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Aluno(models.Model):
 
     nome = models.CharField('Nome do aluno:', max_length=100)
-    ingresso = models.CharField('Ingresso', max_length=100)
+    ingresso = models.DateField('Ingresso', max_length=100)
     nota = models.IntegerField('Nota do aluno:')
 
     def __str__(self):
